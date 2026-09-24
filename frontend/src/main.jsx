e5f561f9b4cbc80e8ad58411125078746cba52a6
Jazil Imran <jazil.imran@gmail.com>
2026-09-24 17:31:21 +0100

    Updated oAuth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const CLIENT_ID = "1705969237-dv264flgt4qerg946jttml8mhrjs4hau.apps.googleusercontent.com"
+const CLIENT_ID = "551742935725-q7spvqubqo952970ue7j5biochpebm08.apps.googleusercontent.com"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
